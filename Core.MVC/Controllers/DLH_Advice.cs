5568f6f0dbd723defc0ba661fc422ad1ba1f66cc
林虎 杜 <1661392274@qq.com>
2020-12-19 11:28:33 +0800

    123

---
+++ Core.MVC/Controllers/DLH_Advice.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Core.MVC.Controllers
+{
+    public class DLH_Advice : Controller
+    {
+        public IActionResult AdviceShow()
+        {
+            return View();
+        }
+
+        public IActionResult AdviceAdd()
+        {
+            return View();
+        }
+
+        public IActionResult AdviceUpdate(int cid)
+        {
+            ViewBag.cases = cid;
+            return View();
+        }
+    }
+}
