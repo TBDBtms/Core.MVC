c42d3c82f9e66ecc307dc8c26b2e2b2afbe9165b
林虎 杜 <1661392274@qq.com>
2020-12-21 16:26:53 +0800

    123

---
+++ Core.MVC/Controllers/DLH_Princ.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Core.MVC.Controllers
+{
+    public class DLH_Princ : Controller
+    {
+        public IActionResult PrincShow()
+        {
+            return View();
+        }
+    }
+}
